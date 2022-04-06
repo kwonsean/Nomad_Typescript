271127aaff373d09447fab4777db2acdfdab4e48
kwonsean <gownn12@naver.com>
2022-04-06 21:54:08 +0900

    part 4 : class
    - 클래스는 JS로 컴파일이 되도 남아있어 사용이 가능하다.
    - 단, public, private나 타입지정 부분은 사라진다.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 export {};
-// 인터페이스는 JS에는 없고 JS에도 영향을 미치지 않음
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
-}
 
-const person = {
-  name: "KSH",
-  age: 90,
-  gender: "male",
-};
+// JS에서는 public private 신경 안씀
+// private인 경우 클래스 외부에서 접근 불가능
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+// constructor 메서드는 class가 시작할때마다 실행됨
+
+const KSH = new Human("KSH", 10, "male");
 
 const sayHi = (person: Human): void => {
   console.log("HI!", person.name);
 };
 
-sayHi(person);
-// 인수로 객체를 넘겨주고 인수로 받는 객체 속 요소들의 타입을 지정한 인터페이스를 통해 타입 체크 가능
+sayHi(KSH);
