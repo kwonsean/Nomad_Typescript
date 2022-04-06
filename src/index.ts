015ecc181d59155386c294e96f5d95088f6e191a
kwonsean <gownn12@naver.com>
2022-04-06 21:29:59 +0900

    part 1 : ?
    - ? 를 통해서 값을 선택적으로 넣어줄 수 있다.
    - 즉 값을 안넣어줘도 에러가 발생하지 않는다.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 export {};
 const name = "KSH";
 
-const sayHi = (name) => {
+const sayHi = (name?) => {
   console.log("HI!", name);
 };
 
 sayHi(name);
+/*
+sayHi();
+1개의 인수가 필요한데 0개를 가져왔습니다.
+ */
+
+sayHi(); // ?를 붙이면 값을 안넣어줘도 오류가 발생하지 않음
