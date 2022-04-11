53043f2b3ed4a7bbc5b6f26bcdc8815a56f88d65
kwonsean <gownn12@naver.com>
2022-04-11 22:49:06 +0900

    feat: Add controller
    $ nest generate controller => nest g co
    - 알아서 컨트롤러를 만들어주고 app.module.ts 파일에 연결도 해줌

--- first-nest/src/app.module.ts
+++ first-nest/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [], // url을 가져오고 함수를 실행시키는 역할
+  controllers: [MoviesController], // url을 가져오고 함수를 실행시키는 역할
   providers: [],
 })
 export class AppModule {}
