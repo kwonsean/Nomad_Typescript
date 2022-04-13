87dff0c9312ed47833a244987261fea7133867b1
kwonsean <gownn12@naver.com>
2022-04-13 22:56:07 +0900

    feat: Learn @Body, @Query
    - @Get 동적으로 사용시 :id 이 뒤로 작성하는 @Get은 동적 아이디로
      인식하기 때문에 위에 써주어야 함!

--- first-nest/src/movies/movies.controller.ts
+++ first-nest/src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,6 +7,7 @@ import {
   Patch,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 // movies가 인수로 들어가 있으면 localhost3000/movies로 가면 getAll함수의 리턴값이 나옴
 // 따라서 controller의 인수 부분이 Entry Point를 컨트롤 함 => 아무것도 안넣으면 localhost3000에서 getAll() 실행
@@ -16,24 +18,42 @@ export class MoviesController {
     return "This fnc'll return all movies";
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie after ${searchingYear} YEAR!`;
+  }
+  // http://localhost:3000/movies/search?year=2000 요청시 입력한 쿼리 값 year를 읽어서 가져옴
+
+  // Get 동적 주소를 사용하면 이후 Get들은 무시된다. (id로 인식한다.)
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This fnc'll return one movie with thd movieId : ${movieId}`;
   }
 
+  // 이렇게 밑에 있으면 This fnc'll return one movie with thd movieId : search 가 리턴됨
+  // @Get('search')
+  // search() {
+  //   return `We are searching for a movie with title`;
+  // }
+
+  // @Body를 통해서 body에 담긴 값을 가져올 수 있다.
   @Post()
-  create() {
-    return "This fnc'll create a movie";
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This fnc'll delete a movie id ${movieId}`;
   }
 
   // Put 도 업데이트를 하지만 전체를 업데이트한다. 반면 Patch는 일부분을 업데이트한다
-  @Patch('/:id')
-  patchMovie(@Param('id') movieId: string) {
-    return `This fnc'll patch a movie id ${movieId}`;
+  @Patch(':id')
+  patchMovie(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
