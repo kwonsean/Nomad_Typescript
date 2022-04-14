bb0d231e8a5a4f10f3834c29f0361d06886ac585
kwonsean <gownn12@naver.com>
2022-04-14 21:08:41 +0900

    feat: Add service
    - nest generate service => nest g s

--- first-nest/src/app.module.ts
+++ first-nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController], // url을 가져오고 함수를 실행시키는 역할
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
