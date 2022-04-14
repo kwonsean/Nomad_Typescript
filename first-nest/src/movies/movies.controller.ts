eda603b2e190c027ad4a3d49b326cb2bbdf78ef7
kwonsean <gownn12@naver.com>
2022-04-14 21:31:41 +0900

    feat: update service

--- first-nest/src/movies/movies.controller.ts
+++ first-nest/src/movies/movies.controller.ts
@@ -6,16 +6,18 @@ import {
   Param,
   Patch,
   Post,
-  Put,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 // movies가 인수로 들어가 있으면 localhost3000/movies로 가면 getAll함수의 리턴값이 나옴
 // 따라서 controller의 인수 부분이 Entry Point를 컨트롤 함 => 아무것도 안넣으면 localhost3000에서 getAll() 실행
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: any) {}
+
   @Get()
-  getAll() {
-    return "This fnc'll return all movies";
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('search')
@@ -26,8 +28,8 @@ export class MoviesController {
 
   // Get 동적 주소를 사용하면 이후 Get들은 무시된다. (id로 인식한다.)
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This fnc'll return one movie with thd movieId : ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   // 이렇게 밑에 있으면 This fnc'll return one movie with thd movieId : search 가 리턴됨
@@ -39,13 +41,12 @@ export class MoviesController {
   // @Body를 통해서 body에 담긴 값을 가져올 수 있다.
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This fnc'll delete a movie id ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   // Put 도 업데이트를 하지만 전체를 업데이트한다. 반면 Patch는 일부분을 업데이트한다
--- first-nest/src/movies/movies.service.ts
+++ first-nest/src/movies/movies.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 
+// 데이터 베이스 역할
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    return this.movies.find((movie) => movie.id === Number(id));
+  }
+
+  deleteOne(id: string): boolean {
+    this.movies.filter((movie) => movie.id !== Number(id));
+    return true;
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
